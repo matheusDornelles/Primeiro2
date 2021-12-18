06c936d47ee05c0e59821e69f70eb0e1252ae759
Matheus <mdmaia91@gmail.com>
2021-12-18 16:11:34 -0300

    Add project files.

---
+++ Primeiro2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Primeiro2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = "hello, ";
+            var b = "World! ";
+            Console.WriteLine( a + b );
+
+
+        }
+    }
+
+}
+
+
